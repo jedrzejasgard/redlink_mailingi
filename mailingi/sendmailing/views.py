ae0f93787a5cca557c831d7a55a1d9cd5c83f179
jedrzej <j.pawlewski@asgard.gifts>
2021-03-08 14:45:43 +0100

    Add ogolne dane o kampanii

--- mailingi/sendmailing/views.py
+++ mailingi/sendmailing/views.py
@@ -33,6 +33,11 @@ def zestawienie_kampanii(request):
 
 
 def szczegoly_kampanii_redlink(id_kampanii, data_wyslania):
+    data_do = data_wyslania.split('-')
+    data_do[-1] = str(int(data_do[-1])+2)
+    if len(data_do[-1]) <= 1:
+        data_do[-1] = '0'+data_do[-1]
+    data_do = '-'.join(data_do)
     try:
         redlink_kampania = zeep.Client('https://redlink.pl/ws/v1/Soap/MailCampaigns/MailCampaigns.asmx?WSDL')
         client = zeep.Client('https://redlink.pl/ws/v1/Soap/Contacts/Groups.asmx?WSDL')
@@ -40,14 +45,14 @@ def szczegoly_kampanii_redlink(id_kampanii, data_wyslania):
                                                                                 strPassword=passw,
                                                                                 strCampaignId=id_kampanii,
                                                                                 dateFrom=str(data_wyslania),
-                                                                                dateTo=str(datetime.date.today()),
+                                                                                dateTo=data_do,
                                                                                 offset=0,
                                                                                 limit=10000)
         response_ctr = redlink_kampania.service.GetMailCampaignCtrReport(strUserName=usr,
                                                                          strPassword=passw,
                                                                          strCampaignId=id_kampanii,
                                                                          dateFrom=str(data_wyslania),
-                                                                         dateTo=str(datetime.date.today()),
+                                                                         dateTo=data_do,
                                                                          offset=0,
                                                                          limit=10000)
         response_data = redlink_kampania.service.GetMailCampaignData(strUserName=usr,
@@ -57,14 +62,14 @@ def szczegoly_kampanii_redlink(id_kampanii, data_wyslania):
                                                                        strPassword=passw,
                                                                        strCampaignId=id_kampanii,
                                                                        dateFrom=str(data_wyslania),
-                                                                       dateTo=str(datetime.date.today()),
+                                                                       dateTo=data_do,
                                                                        offset=0,
                                                                        limit=10000)
         response_unsub = redlink_kampania.service.GetMailCampaignUnregistrationsReport(strUserName=usr,
                                                                                        strPassword=passw,
                                                                                        strCampaignId=id_kampanii,
                                                                                        dateFrom=str(data_wyslania),
-                                                                                       dateTo=str(datetime.date.today()),
+                                                                                       dateTo=data_do,
                                                                                        offset=0,
                                                                                        limit=10000 )
         r_json_bounce = zeep.helpers.serialize_object(response_bounce)
@@ -89,9 +94,10 @@ def szczegoly_kampanii_redlink(id_kampanii, data_wyslania):
         dostarczone_wiadomosci = int(r_json_groupcount['Data']) - int(bounces)
         ctr = round(clicks*100/dostarczone_wiadomosci, 2)
         open_rate = round(otwarte_maile*100/dostarczone_wiadomosci, 2)
-        dane = [ctr, open_rate, dostarczone_wiadomosci, un_sub]
+        dane = [ctr, open_rate, dostarczone_wiadomosci, un_sub, clicks, dostarczone_wiadomosci, otwarte_maile]
     except:
         dane = ['blad', 'blad', 'blad', 'blad']
+    print(dane)
     return dane
 
 
@@ -99,16 +105,102 @@ def detale_kampanii(request):
     if request.method == "POST":
         id_kampanii = (request.POST['form_id_kampanii'])
         kampanie_model = KampaniaRedlink.objects.filter(id=str(id_kampanii)).values()
-        mailing_dane = {}
-        for kampania in kampanie_model:
-            data_wyslania = str(kampania['kiedy_wyslany'].split(' ')[0])
-            for item in kampania:
-                item2 = item.split('_')
-                if item2[0] == 'redlink':
-                    id_redlink = kampania[item]
-                    mailing_dane[id_redlink] = (szczegoly_kampanii_redlink(id_redlink, data_wyslania))
+        nazwa_kampanii = kampanie_model[0]['nazwa_kampanii']
+        handlowcy_kampanii = Handlowiec.objects.filter(nazwa_kampanii=nazwa_kampanii).values()
+
+        data_wyslania = str(kampanie_model[0]['kiedy_wyslany'].split(' ')[0])
+
+        data_wyslania = '2021-03-01'
+
+        wyniki_handlowcy_pl = {}
+        wyniki_handlowcy_en = {}
+        wyniki_handlowcy_de = {}
+        wyniki_handlowcy_fr = {}
+        dostarczone_wiadomosci_pl = 0
+        dostarczone_wiadomosci_en = 0
+        dostarczone_wiadomosci_de = 0
+        dostarczone_wiadomosci_fr = 0
+        un_sub_pl = 0
+        un_sub_en = 0
+        un_sub_de = 0
+        un_sub_fr = 0
+        clicks_pl = 0
+        clicks_en = 0
+        clicks_de = 0
+        clicks_fr = 0
+        opens_pl = 0
+        opens_en = 0
+        opens_de = 0
+        opens_fr = 0
+        for handlowiec in handlowcy_kampanii:
+            id_redlink = handlowiec['redlink_id']
+            if handlowiec['jezyk'] == 'pl':
+                wyniki_handlowcy_pl[handlowiec['imie_nazwisko']] = (id_redlink, szczegoly_kampanii_redlink(id_redlink, data_wyslania))
+            elif handlowiec['jezyk'] == 'en':
+                wyniki_handlowcy_en[handlowiec['imie_nazwisko']] = (id_redlink, szczegoly_kampanii_redlink(id_redlink, data_wyslania))
+            elif handlowiec['jezyk'] == 'de':
+                wyniki_handlowcy_de[handlowiec['imie_nazwisko']] = (id_redlink, szczegoly_kampanii_redlink(id_redlink, data_wyslania))
+            elif handlowiec['jezyk'] == 'fr':
+                wyniki_handlowcy_fr[handlowiec['imie_nazwisko']] = (id_redlink, szczegoly_kampanii_redlink(id_redlink, data_wyslania))
+        for handlowiec in wyniki_handlowcy_pl:
+            handlowiec = wyniki_handlowcy_pl[handlowiec][1]
+            un_sub_pl += handlowiec[3]
+            clicks_pl += handlowiec[4]
+            dostarczone_wiadomosci_pl += handlowiec[5]
+            opens_pl += handlowiec[6]
+        ctr_pl = round(clicks_pl * 100 / dostarczone_wiadomosci_pl, 2)
+        open_rate_pl = round(opens_pl * 100 / dostarczone_wiadomosci_pl, 2)
+        for handlowiec in wyniki_handlowcy_en:
+            handlowiec = wyniki_handlowcy_en[handlowiec][1]
+            un_sub_en += handlowiec[3]
+            clicks_en += handlowiec[4]
+            dostarczone_wiadomosci_en += handlowiec[5]
+            opens_en += handlowiec[6]
+        ctr_en = round(clicks_en * 100 / dostarczone_wiadomosci_en, 2)
+        open_rate_en = round(opens_en * 100 / dostarczone_wiadomosci_en, 2)
+        for handlowiec in wyniki_handlowcy_de:
+            handlowiec = wyniki_handlowcy_de[handlowiec][1]
+            un_sub_de += handlowiec[3]
+            clicks_de += handlowiec[4]
+            dostarczone_wiadomosci_de += handlowiec[5]
+            opens_de += handlowiec[6]
+        ctr_de = round(clicks_pl * 100 / dostarczone_wiadomosci_de, 2)
+        open_rate_de = round(opens_de * 100 / dostarczone_wiadomosci_de, 2)
+        for handlowiec in wyniki_handlowcy_fr:
+            handlowiec = wyniki_handlowcy_fr[handlowiec][1]
+            un_sub_fr += handlowiec[3]
+            clicks_fr += handlowiec[4]
+            dostarczone_wiadomosci_fr += handlowiec[5]
+            opens_fr += handlowiec[6]
+        ctr_fr = round(clicks_fr * 100 / dostarczone_wiadomosci_fr, 2)
+        open_rate_fr = round(opens_fr * 100 / dostarczone_wiadomosci_fr, 2)
+        kampania_model = KampaniaRedlink.objects.get(id=str(id_kampanii))
+        kampania_model.ctr_pl = ctr_pl
+        kampania_model.ctr_en = ctr_en
+        kampania_model.ctr_de = ctr_de
+        kampania_model.ctr_fr = ctr_fr
+        kampania_model.open_rate_pl = open_rate_pl
+        kampania_model.open_rate_en = open_rate_en
+        kampania_model.open_rate_de = open_rate_de
+        kampania_model.open_rate_fr = open_rate_fr
+        kampania_model.dostarczone_wiadomosci_pl = dostarczone_wiadomosci_pl
+        kampania_model.dostarczone_wiadomosci_en = dostarczone_wiadomosci_en
+        kampania_model.dostarczone_wiadomosci_de = dostarczone_wiadomosci_de
+        kampania_model.dostarczone_wiadomosci_fr = dostarczone_wiadomosci_fr
+        kampania_model.un_sub_pl = un_sub_pl
+        kampania_model.un_sub_en = un_sub_en
+        kampania_model.un_sub_de = un_sub_de
+        kampania_model.un_sub_fr = un_sub_fr
+        kampania_model.save(update_fields=['ctr_pl', 'ctr_en', 'ctr_de', 'ctr_fr', 'open_rate_pl', 'open_rate_en',
+                                           'open_rate_de', 'open_rate_fr','dostarczone_wiadomosci_pl',
+                                           'dostarczone_wiadomosci_en', 'dostarczone_wiadomosci_de',
+                                           'dostarczone_wiadomosci_fr', 'un_sub_pl', 'un_sub_en', 'un_sub_de',
+                                           'un_sub_fr'])
     return render(request, 'detale_kampanii.html', {'szczegoly': kampanie_model,
-                                                    'dict_mailingi_szczegoly': mailing_dane})
+                                                    'handlowcy_pl': wyniki_handlowcy_pl,
+                                                    'handlowcy_en': wyniki_handlowcy_en,
+                                                    'handlowcy_de': wyniki_handlowcy_de,
+                                                    'handlowcy_fr': wyniki_handlowcy_fr})
 
 
 def wyslij_test(request):
